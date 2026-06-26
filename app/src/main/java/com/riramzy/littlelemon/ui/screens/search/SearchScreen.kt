b9899482431543d5d2f5ebdf88db39cd8fff25c4
riramzy <ramzyi081@gmail.com>
2026-06-26 17:32:58 +0300

    refactor: improve theme consistency in SearchScreen
    - `SearchScreen`: update `Scaffold` to use `MaterialTheme.colorScheme.background` consistently instead of conditional logic
    - Remove unused imports including `isSystemInDarkTheme` and `Color`

--- app/src/main/java/com/riramzy/littlelemon/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/riramzy/littlelemon/ui/screens/search/SearchScreen.kt
@@ -1,7 +1,6 @@
 package com.riramzy.littlelemon.ui.screens.search
 
 import android.content.res.Configuration
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,7 +17,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -90,11 +88,7 @@ fun SearchScreenContent(
             )
         },
         floatingActionButtonPosition = FabPosition.Center,
-        containerColor = if (isSystemInDarkTheme()) {
-            MaterialTheme.colorScheme.background
-        } else {
-            Color.White
-        },
+        containerColor = MaterialTheme.colorScheme.background,
         modifier = Modifier.statusBarsPadding()
     ) { innerPadding ->
 
